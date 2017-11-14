6b95b108e24c0be3733694eeb2211bfb646c77f5
Kritsana <dumtk1@gmail.com>
2017-11-14 23:42:25 +0700

    Edit 14112017

--- app/src/main/java/beacon/project/com/beaconsystem/MainActivity.java
+++ app/src/main/java/beacon/project/com/beaconsystem/MainActivity.java
@@ -1,8 +1,14 @@
 package beacon.project.com.beaconsystem;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,6 +21,12 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private BluetoothManager btManager;
+    private BluetoothAdapter btAdapter;
+    private Handler scanHandler = new Handler();
+    private int scan_interval_ms = 5000;
+    private boolean isScanning = false;
+    private String TAG = "Main Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +44,45 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    private void  initMainApps(){
+        // init BLE
+        btManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        btAdapter = btManager.getAdapter();
+        scanHandler.post(scanRunnable);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            initMainApps();
+        }catch (Exception e){
+            Log.e(TAG,e.getMessage());
+        }
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try{
+            scanHandler.removeCallbacksAndMessages(null);
+        }catch (Exception e){
+            Log.e(TAG,e.getMessage());
+        }
+
     }
 
     @Override
@@ -85,5 +126,87 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer =  findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    private Runnable scanRunnable = new Runnable()
+    {
+        @Override
+        public void run() {
+
+            if (isScanning)
+            {
+                if (btAdapter != null)
+                {
+                    btAdapter.stopLeScan(leScanCallback);
+                }
+            }
+            else
+            {
+                if (btAdapter != null)
+                {
+                    btAdapter.startLeScan(leScanCallback);
+                }
+            }
+
+            isScanning = !isScanning;
+
+            scanHandler.postDelayed(this, scan_interval_ms);
+        }
+    };
+
+    private BluetoothAdapter.LeScanCallback leScanCallback = new BluetoothAdapter.LeScanCallback()
+    {
+        @Override
+        public void onLeScan(final BluetoothDevice device, final int rssi, final byte[] scanRecord)
+        {
+            int startByte = 2;
+            boolean patternFound = false;
+            while (startByte <= 5)
+            {
+                if (    ((int) scanRecord[startByte + 2] & 0xff) == 0x02 && //Identifies an iBeacon
+                        ((int) scanRecord[startByte + 3] & 0xff) == 0x15)
+                { //Identifies correct data length
+                    patternFound = true;
+                    break;
+                }
+                startByte++;
+            }
+
+            if (patternFound)
+            {
+                //Convert to hex String
+                byte[] uuidBytes = new byte[16];
+                System.arraycopy(scanRecord, startByte + 4, uuidBytes, 0, 16);
+                String hexString = bytesToHex(uuidBytes);
+
+                //UUID detection
+                String uuid =  hexString.substring(0,8) + "-" +
+                        hexString.substring(8,12) + "-" +
+                        hexString.substring(12,16) + "-" +
+                        hexString.substring(16,20) + "-" +
+                        hexString.substring(20,32);
+
+                // major
+                final int major = (scanRecord[startByte + 20] & 0xff) * 0x100 + (scanRecord[startByte + 21] & 0xff);
+
+                // minor
+                final int minor = (scanRecord[startByte + 22] & 0xff) * 0x100 + (scanRecord[startByte + 23] & 0xff);
+
+                Log.i("BEACON","UUID: " +uuid + "\\nmajor: " +major +"\\nminor" +minor);
+            }
+
+        }
+    };
+
+    static final char[] hexArray = "0123456789ABCDEF".toCharArray();
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 }
